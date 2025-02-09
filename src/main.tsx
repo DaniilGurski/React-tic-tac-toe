3af48602cbfda9266cf4b89a01f5cd5c8465cebe
DaniilGurski <daniil.gurski@elev.ga.ntig.se>
2025-02-09 15:09:15 +0100

    add alias imports.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./style.css";
+import "@/style.css";
 
 createRoot(document.getElementById("root")!).render(<StrictMode></StrictMode>);
